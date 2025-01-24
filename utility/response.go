992597e5e82f0982a4cbc708d37e01d94a7bddd6
rlaxogh5079 <rlaxogh507906@gmail.com>
2025-01-24 21:32:01 +0900

    ♻️ Response 구조체 User 필드 추가

--- utility/response.go
+++ utility/response.go
@@ -8,5 +8,6 @@ type Response struct {
 	Message    string                 `json:"message"`
 	StatusCode uint                   `json:"code"`
 	Token      string                 `json:"token"`
+	User       model.User             `json:"user"`
 	Error      *model.BookPocketError `json:"error"`
 }
